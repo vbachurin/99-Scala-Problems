16e009e0416d9e2f7034308bf6121b21c674b0c2
Vlad Bachurin <vbachurin@users.noreply.github.com>
2017-05-13 14:39:13 +0300

    Update P01.scala

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -2,6 +2,12 @@ package org.p99.scala
 
 object P01 {
 
-  // TODO
+  def last[A](xs: List[A]): A = xs match {
+    case List(x) => x
+    case h :: t => last(t)
+    case Nil => throw new NoSuchElementException("Last of empty list")
+  }
+
+  // last(List(1, 1, 2, 3, 5, 8))
 
 }
